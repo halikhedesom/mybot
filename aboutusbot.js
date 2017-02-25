5afe5884f4825aa79b2314ed3bde0c301af763eb
Someshwar Halikhede <Someshwar Halikhede>
2017-02-25 23:45:54 +0530

    updated bot creds

--- aboutusbot.js
+++ aboutusbot.js
@@ -9,10 +9,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function(){
 
 //create a chatbot
 var connector = new builder.ChatConnector({
-    //appId: process.env.MICROSOFT_APP_ID,
-    appId:'fb93c7f8-a660-4715-9874-aa2b9e34de79',
-    //appPassword: process.env.MICROSOFT_APP_PASSWORD
-    appPassword: 'CY7AdZcYxWeozBscarDyGQi'
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
 var bot = new builder.UniversalBot(connector);
